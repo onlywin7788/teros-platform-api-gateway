3202fe646496635de269fc538db0585d13749c08
navana <onlywin7788@gmail.com>
2021-12-27 00:05:51 +0900

    commit

--- src/main/java/com/teros/api_gateway/Application.java
+++ src/main/java/com/teros/api_gateway/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TerosPlatformApiGatewayApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TerosPlatformApiGatewayApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
