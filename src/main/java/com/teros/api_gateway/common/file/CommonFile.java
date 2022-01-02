db72b450231e6f3440fca299d2143de6dc9afe60
navana <onlywin7788@gmail.com>
2022-01-02 18:22:22 +0900

    commit

---
+++ src/main/java/com/teros/api_gateway/common/file/CommonFile.java
@@ -0,0 +1,53 @@
+package com.teros.api_gateway.common.file;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommonFile {
+    public boolean fileExist(String filePath) {
+        if (Files.exists(Paths.get(filePath)))
+            return true;
+        else
+            return false;
+    }
+
+    public List<String> getFileList(String dir)
+    {
+        List fileList = new ArrayList();
+
+        for (File info : FileUtils.listFiles(new File(dir), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE)) {
+            fileList.add(info.getName());
+        }
+        return fileList;
+    }
+
+    public byte[] readFile(String ConfigPath) {
+        byte[] contents = null;
+        try {
+            File file = new File(ConfigPath);
+            contents = FileUtils.readFileToByteArray(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contents;
+    }
+
+    public void writeFile(String filePath, String contents) {
+        try {
+            FileOutputStream outputStream = new FileOutputStream(filePath);
+            byte[] strToBytes = contents.getBytes();
+            outputStream.write(strToBytes);
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/teros/api_gateway/schedule/scheduleRoute.java
+++ src/main/java/com/teros/api_gateway/schedule/scheduleRoute.java
@@ -1,9 +1,15 @@
 package com.teros.api_gateway.schedule;
 
+import com.teros.api_gateway.common.network.RemoteNetworkCall;
+import com.teros.ext.common.file.CommonFile;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -12,8 +18,34 @@ public class scheduleRoute {
     @Value("${extra.env.teros_home}")
     private String terosHomeEnv;
 
+
+    @Value("${server.port}")
+    private String servicePort;
+
     @Scheduled(fixedDelay = 3000)
     public void updateRouteRule() {
-        log.info("update rule");
+
+        CommonFile commonFile = new CommonFile();
+        RemoteNetworkCall remoteNetworkCall = new RemoteNetworkCall();
+        String configDir = String.format("%s/api-service/config", terosHomeEnv);
+
+        // search config files
+        File dir = new File(configDir);
+        File files[] = dir.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+            String configFilePath = files[i].toString();
+
+            // create route
+            String contents = new String(commonFile.readFile(configFilePath));
+            remoteNetworkCall.requestRestEntity(
+                    String.format("http://127.0.0.1:%s/actuator/gateway/routes/route_%d", servicePort, i+1)
+                    , HttpMethod.POST, contents);
+
+        }
+        // refresh route
+        remoteNetworkCall.requestRestEntity(
+                String.format("http://127.0.0.1:%s/actuator/gateway/refresh", servicePort)
+                , HttpMethod.POST, "");
     }
 }
